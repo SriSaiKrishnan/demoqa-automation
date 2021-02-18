8a9abbddff2fc403c6e2316f9aeffe6441f35024
Saikrishnan <krishnansai99@gmail.com>
2021-02-18 22:50:37 +0530

    Tools QA

--- src/main/java/com/toolsqa/base/BasePage.java
+++ src/main/java/com/toolsqa/base/BasePage.java
@@ -12,8 +12,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BasePage {
 	
